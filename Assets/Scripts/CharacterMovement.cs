0bf56f1dced400fb94945300b6a2d19bdc399968
Rizosha <Pheobe6970>
2021-11-04 01:15:08 +0000

    Initial

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+ 
+    
+    public CharacterController controller;
+    
+    public float speed = 12f;
+    
+
+   
+    void Update()
+    {
+        // takes input from unity controller 
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        // creates a transform based off input
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+        
+        // moves the character using speed and delta time 
+        
+        controller.Move(move * speed * Time.deltaTime);
+
+    }
+}
