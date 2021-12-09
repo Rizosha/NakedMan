04c302a4db99065a3cf697e2fc97e737b0c444b0
Rizosha <ashleytjames2110@gmail.com>
2021-12-09 02:15:08 +0000

    Updated some animations

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,15 +9,34 @@ public class CharacterMovement : MonoBehaviour
     public CharacterController controller;
     
     public float speed = 12f;
+    public float gravity = -9.81f;
     
+    private Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    private Vector3 moveDir;
 
    
     void Update()
     {
-        // takes input from unity controller 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        // takes input from unity controller
         
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(x, z).normalized;
+        
+       
         
         // creates a transform based off input
         
@@ -27,5 +46,11 @@ public class CharacterMovement : MonoBehaviour
         
         controller.Move(move * speed * Time.deltaTime);
 
+        
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.fixedDeltaTime); 
+        
+      
     }
 }
---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    /*CharacterMovement moveScript;
+
+    public float dashSpeed;
+    public float dashTime;
+    
+    void Start()
+    {
+        moveScript = GetComponent<CharacterMovement>();
+        StartCoroutine(DashMovementRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            StartCoroutine(DashMovementRoutine());
+        }
+    }
+
+    IEnumerator DashMovementRoutine()
+    {
+        yield return
+            WaitForSeconds(2);
+        isDashing = true;
+
+
+    }*/
+
+
+    
+    
+}
